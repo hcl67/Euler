9a5515dfd4dd23beb6109bc0ba02a677798a2893
hcl67 <djinn.luu@gmail.com>
2021-06-29 22:51:04 +0100

    Update euler81.py

--- euler81.py
+++ euler81.py
@@ -1,13 +1,14 @@
-
 #mxraw = [[131,673,234,103,18],[201,96,342,965,150],[630,803,746,422,111],[537,699,497,121,956],[805,732,524,37,331]]
-#dim = 5
+#dim1 = dim2 = 5
+
+_DATA = "p081_matrix.txt"
 
 
 import csv
 
-dim = 80
+dim1 = dim2 = 80
 mxraw = []
-with open("p081_matrix.txt") as csvfile:
+with open(_DATA) as csvfile:
     x = csv.reader(csvfile)
     for row in x:
         mxraw.append([int(i) for i in row])
@@ -16,21 +17,23 @@ with open("p081_matrix.txt") as csvfile:
 #print(mxraw[0][0])
 
 
-mxpath = [[0] * dim for i in range(dim)]
-mxpath[0][0] = mxraw[0][0]
-for n in range(1,dim):
-    for j in range(n+1):
-        if j==0:
-            mxpath[j][n-j] = mxraw[j][n-j] + mxpath[j][n-j-1]
-        elif j==n:
-            mxpath[j][n-j] = mxraw[j][n-j] + mxpath[j-1][n-j]
-        else:
-            mxpath[j][n-j] = mxraw[j][n-j] + min(mxpath[j][n-j-1],mxpath[j-1][n-j])
-#print(mxpath)     
-for n in range(1,dim):
-    for j in range(dim-n):
-        mxpath[dim-1-j][j+n] = mxraw[dim-1-j][j+n] + min(mxpath[dim-1-j-1][j+n] ,mxpath[dim-1-j][j+n-1] )
+mxpath = [[0] * dim2 for i in range(dim1)]
 
-print(mxpath[dim-1][dim-1])
+
+
+for i in range(dim1):
+    for j in range(dim2):
+        if i==0 and j==0:
+            mxpath[i][j] = mxraw[i][j]
+        elif i==0 and j>0:
+            mxpath[i][j] = mxraw[i][j] + mxpath[i][j-1]
+        elif i>0 and j==0:
+            mxpath[i][j] = mxraw[i][j] + mxpath[i-1][j]
+        else:
+            mxpath[i][j] = mxraw[i][j] + min(mxpath[i][j-1], mxpath[i-1][j])
+#print(mxpath)     
+
+print(mxpath[dim1-1][dim2-1])
+       
        
             
