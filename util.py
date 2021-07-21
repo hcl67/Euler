cb8b757122797cb35564afa24bf665c439e46e12
hcl67 <djinn.luu@gmail.com>
2021-07-21 13:28:53 +0100

    Create util.py

---
+++ util.py
@@ -0,0 +1,30 @@
+f = open("prime100000.txt", "r")
+plist = []
+for line in f:
+    plist += list(map(int,line.rstrip("\n").split(" ")))
+f.close()
+
+def isprime(n):
+    if n == 1:
+        return False
+    for p in plist:
+        if p*p>n:
+            break
+        elif n%p == 0:
+            return False
+    return True
+  
+  
+  def pfdeco(n):
+    pf = []
+    pn = 0
+    p = plist[pn]
+    while n>1:
+        if n%p == 0:
+            pf += [p]
+            n //= p
+        else:
+            pn += 1
+            p = plist[pn]
+    return pf
+    
