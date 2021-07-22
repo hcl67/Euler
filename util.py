4ef56b25c140ff983d519ef6b9e52ba7a0c64d97
hcl67 <djinn.luu@gmail.com>
2021-07-22 17:41:43 +0100

    Update util.py

--- util.py
+++ util.py
@@ -27,12 +27,3 @@ def pfdeco(n):
             p = plist[pn]
     return pf
     
-    
-def gcd(a,b):
-    if a>b:
-        a,b = b,a
-    c = b%a
-    if c == 0:
-        return a
-    else:
-        return gcd(c,a)    
