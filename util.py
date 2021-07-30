4e70449688f57130e793c24ee7739ecba22bd743
hcl67 <djinn.luu@gmail.com>
2021-07-30 23:28:32 +0100

    Update util.py

--- util.py
+++ util.py
@@ -105,3 +105,13 @@ def isprime_mr(n):
         else:
             return False
     return True    
+
+
+def max_subarray(numbers):
+    """Find the largest sum of any contiguous subarray. Kadane's algorithm"""
+    best_sum = 0  # or: float('-inf')
+    current_sum = 0
+    for x in numbers:
+        current_sum = max(0, current_sum + x)
+        best_sum = max(best_sum, current_sum)
+    return best_sum
